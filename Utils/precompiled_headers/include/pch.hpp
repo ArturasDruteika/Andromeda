5bebdc25953b5d93dd3d9256893a0b13a5dc5e2c
ArtūrasDrūteika <arturas.druteika@gmail.com>
2024-12-31 14:36:16 +0200

    Add precompiled headers

---
+++ Utils/precompiled_headers/include/pch.hpp
@@ -0,0 +1,18 @@
+#ifndef UTILS__PCH__HPP
+#define UTILS__PCH__HPP
+
+
+// Standard library headers
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <cstdint>
+
+
+#endif // UTILS__PCH__HPP
