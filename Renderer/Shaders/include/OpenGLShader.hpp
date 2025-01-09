4908d45e9da9b9461e04601c7938ea7af1963a2b
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-01-09 09:26:53 +0200

    Add binding operations

--- Renderer/Shaders/include/OpenGLShader.hpp
+++ Renderer/Shaders/include/OpenGLShader.hpp
@@ -3,6 +3,7 @@
 
 
 #include "pch.hpp"
+#include <glm/glm.hpp>
 
 
 namespace Andromeda
@@ -15,14 +16,22 @@ namespace Andromeda
 			OpenGLShader(const std::string& vertexCode, const std::string& fragmentCode);
 			~OpenGLShader();
 
+			void Bind() const;
+			void UnBind() const;
+			void SetUniform(const std::string& name, float value);
+			void SetUniform(const std::string& name, const glm::mat4& matrix);
+
 			unsigned int GetProgram();
 
 		private:
 			unsigned int CompileShader(unsigned int type, const std::string& shaderSource);
 			unsigned int CreateShaderProgram(const std::string& vertexCode, const std::string& fragmentCode);
+			int GetUniformLocation(const std::string& name);
 			void CheckCompileErrors(unsigned int shader, int type);
+			void CheckProgramErrors(unsigned int program);
 
 			unsigned int m_program;
+			std::unordered_map<std::string, int> m_uniformLocationCache;
 		};
 	}
 }
--- Renderer/Shaders/src/OpenGLShader.cpp
+++ Renderer/Shaders/src/OpenGLShader.cpp
@@ -1,5 +1,7 @@
 #include "../include/OpenGLShader.hpp"
 #include "glad/gl.h"
+#include <glm/gtc/type_ptr.hpp>
+
 
 
 namespace Andromeda
@@ -17,6 +19,34 @@ namespace Andromeda
 			{
 				glDeleteProgram(m_program);
 				m_program = 0;
+			}
+		}
+
+		void OpenGLShader::Bind() const
+		{
+			glUseProgram(m_program);
+		}
+
+		void OpenGLShader::UnBind() const
+		{
+			glUseProgram(0);
+		}
+
+		void OpenGLShader::SetUniform(const std::string& name, float value)
+		{
+			int location = GetUniformLocation(name);
+			if (location != -1)
+			{
+				glUniform1f(location, value);
+			}
+		}
+
+		void OpenGLShader::SetUniform(const std::string& name, const glm::mat4& matrix)
+		{
+			int location = GetUniformLocation(name);
+			if (location != -1)
+			{
+				glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 			}
 		}
 
@@ -45,11 +75,31 @@ namespace Andromeda
 			glAttachShader(program, fragmentShader);
 			glLinkProgram(program);
 			glValidateProgram(program);
+			CheckProgramErrors(program);
 
 			glDeleteShader(vertexShader);
 			glDeleteShader(fragmentShader);
 
 			return program;
+		}
+
+		int OpenGLShader::GetUniformLocation(const std::string& name)
+		{
+			if (m_uniformLocationCache.find(name) != m_uniformLocationCache.end())
+			{
+				return m_uniformLocationCache[name];
+			}
+
+			int location = glGetUniformLocation(m_program, name.c_str());
+			if (location == -1)
+			{
+				spdlog::warn("Uniform '{}' not found or not used in shader.", name);
+			}
+			else
+			{
+				m_uniformLocationCache[name] = location;
+			}
+			return location;
 		}
 
 		void OpenGLShader::CheckCompileErrors(unsigned int shader, int type)
@@ -67,6 +117,21 @@ namespace Andromeda
 				glDeleteShader(shader);
 			}
 			return;
+		}
+
+		void OpenGLShader::CheckProgramErrors(unsigned int program)
+		{
+			int success;
+			glGetProgramiv(program, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				int length;
+				glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
+				char* message = (char*)alloca(length * sizeof(char));
+				glGetProgramInfoLog(program, length, nullptr, message);
+				spdlog::error("Shader program linking error: {}", message);
+				glDeleteProgram(program);
+			}
 		}
 	}
 }
