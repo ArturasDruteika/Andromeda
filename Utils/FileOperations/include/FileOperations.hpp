1ff660b5f1b37109b00d64115e2a052ef25d91b8
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-01-09 21:43:54 +0200

    Create file operations class

---
+++ Utils/FileOperations/include/FileOperations.hpp
@@ -0,0 +1,22 @@
+#ifndef UTILS__FILE_OPERATIONS__HPP
+#define UTILS__FILE_OPERATIONS__HPP
+
+
+#include "../../PrecompiledHeaders/include/pch.hpp"
+
+
+namespace Andromeda
+{
+	namespace Utils
+	{
+		class FileOperations
+		{
+		public:
+			static std::string LoadFileAsString(const std::string& filePath);
+		};
+	}
+}
+
+
+
+#endif // UTILS__FILE_OPERATIONS__HPP
