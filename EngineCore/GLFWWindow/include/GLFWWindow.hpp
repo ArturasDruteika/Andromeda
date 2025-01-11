c21b5155426560c57ffcc743293d6caa8dcf1d7e
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-01-11 18:28:35 +0200

    Preventing copying or moving for window class

--- EngineCore/GLFWWindow/include/GLFWWindow.hpp
+++ EngineCore/GLFWWindow/include/GLFWWindow.hpp
@@ -15,6 +15,11 @@ namespace Andromeda
 			GLFWWindow(int width = 640, int height = 640, const std::string& windowName = "Andromeda Window", bool initWindow = true);
 			~GLFWWindow();
 
+			GLFWWindow(const GLFWWindow& other) = delete;	// Prevent Copy Constructor
+			GLFWWindow& operator=(const GLFWWindow& other) = delete;	// Prevent Copy assignment
+			GLFWWindow(GLFWWindow&& other) noexcept = delete;	// Prevent Move constructor
+			GLFWWindow& operator=(const GLFWWindow&& other) noexcept = delete;	//Prevent Move assignment
+
 			void Init();
 			void DeInit();
 			void RunMainLoop();
