74d9d33175502f556c1e1f8edd379ce431bf607d
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-01-18 15:45:50 +0200

    Change parameter type for the interface class
    Refactor code

--- Renderer/Renderers/include/OpenGLRendererImpl.hpp
+++ Renderer/Renderers/include/OpenGLRendererImpl.hpp
@@ -1,43 +1,45 @@
-	#ifndef RENDERER__OPENGL_RENDERER_IMPL__HPP
-	#define RENDERER__OPENGL_RENDERER_IMPL__HPP
+#ifndef RENDERER__OPENGL_RENDERER_IMPL__HPP
+#define RENDERER__OPENGL_RENDERER_IMPL__HPP
 
 
-	#include "../include/OpenGLRenderer.hpp"
-	#include "../../Shaders/include/OpenGLShader.hpp"
+#include "../include/OpenGLRenderer.hpp"
+#include "../../Shaders/include/OpenGLShader.hpp"
+#include <glad/gl.h>
 
 
-	namespace Andromeda
+
+namespace Andromeda
+{
+	namespace Renderer
 	{
-		namespace Renderer
+		class OpenGLRenderer::OpenGLRendererImpl
 		{
-			class OpenGLRenderer::OpenGLRendererImpl
-			{
-			public:
-				OpenGLRendererImpl();
-				~OpenGLRendererImpl();
+		public:
+			OpenGLRendererImpl();
+			~OpenGLRendererImpl();
 
-				OpenGLRendererImpl(const OpenGLRendererImpl& other) = delete;	// Prevent Copy Constructor
-				OpenGLRendererImpl& operator=(const OpenGLRendererImpl& other) = delete;	// Prevent Copy Assignment
-				OpenGLRendererImpl(OpenGLRendererImpl&& other) noexcept = delete;	// Prevent Move Constructor
-				OpenGLRendererImpl& operator=(OpenGLRendererImpl&& other) noexcept = delete;	// Prevent Move Assignment
+			OpenGLRendererImpl(const OpenGLRendererImpl& other) = delete;	// Prevent Copy Constructor
+			OpenGLRendererImpl& operator=(const OpenGLRendererImpl& other) = delete;	// Prevent Copy Assignment
+			OpenGLRendererImpl(OpenGLRendererImpl&& other) noexcept = delete;	// Prevent Move Constructor
+			OpenGLRendererImpl& operator=(OpenGLRendererImpl&& other) noexcept = delete;	// Prevent Move Assignment
 
-				bool IsInitialized() const;
-				void Initialize(GLADloadfunc load);
-				void RenderFrame(const Environment::OpenGLScene& scene);
-				void Shutdown();
+			bool IsInitialized() const;
+			void Initialize(const char* name);
+			void RenderFrame(const Environment::OpenGLScene& scene);
+			void Shutdown();
 
-			private:
-				void LoadGlad(GLADloadfunc load);
-				void CreateShader();
+		private:
+			void LoadGlad(GLADloadfunc load);
+			void CreateShader();
 				 
-				bool m_isInitialized;
-				// VBO - vertex buffer obj
-				// VAO - vertex array obj
-				unsigned int m_VBO, m_VAO;
-				OpenGLShader* m_shader;
-			};
-		}
+			bool m_isInitialized;
+			// VBO - vertex buffer obj
+			// VAO - vertex array obj
+			unsigned int m_VBO, m_VAO;
+			OpenGLShader* m_shader;
+		};
 	}
+}
 
 
-	#endif // RENDERER__OPENGL_RENDERER_IMPL__HPP
+#endif // RENDERER__OPENGL_RENDERER_IMPL__HPP
