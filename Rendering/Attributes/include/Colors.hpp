344e604007daab62048a45fcaaf3764f7fd68911
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-03-19 22:46:31 +0200

    Add point 3d and color structs

---
+++ Rendering/Attributes/include/Colors.hpp
@@ -0,0 +1,20 @@
+#ifndef ENVIRONMENT__POINTS__HPP
+#define ENVIRONMENT__POINTS__HPP
+
+
+namespace Andromeda
+{
+	namespace Rendering
+	{
+		struct Color
+		{
+			float r;
+			float g;
+			float b;
+			float a;
+		};
+	}
+}
+
+
+#endif // ENVIRONMENT__ENVIRONMENT__HPP
---
+++ Rendering/Attributes/include/Points.hpp
@@ -0,0 +1,19 @@
+#ifndef ENVIRONMENT__COLORS__HPP
+#define ENVIRONMENT__COLORS__HPP
+
+
+namespace Andromeda
+{
+	namespace Rendering
+	{
+		struct Point3D
+		{
+			float x;
+			float y;
+			float z;
+		};
+	}
+}
+
+
+#endif // ENVIRONMENT__ENVIRONMENT__HPP
