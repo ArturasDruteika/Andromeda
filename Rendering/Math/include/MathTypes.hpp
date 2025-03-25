f3985286becef39bb0940d855ea07be44c42b33f
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-03-25 21:51:35 +0200

    Add math types structs

---
+++ Rendering/Math/include/MathTypes.hpp
@@ -0,0 +1,21 @@
+#ifndef RENDERING__MATH_TYPES__HPP
+#define RENDERING__MATH_TYPES__HPP
+
+
+#include "pch.hpp"
+
+
+namespace Andromeda
+{
+	namespace Rendering
+	{
+		struct Mat4
+		{
+			Point3D position;
+			Color color;
+		};
+	}
+}
+
+
+#endif // RENDERING__MATH_TYPES__HPP
