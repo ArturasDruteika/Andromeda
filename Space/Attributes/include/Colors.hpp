93bc1fb29d03601e2298603aa0af7b1cb5aef275
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-04-28 22:44:41 +0300

    Add default values

--- Space/Attributes/include/Colors.hpp
+++ Space/Attributes/include/Colors.hpp
@@ -8,10 +8,10 @@ namespace Andromeda
 	{
         struct Color
         {
-            float r;
-            float g;
-            float b;
-            float a;
+            float r = 1.0f;
+            float g = 1.0f;
+            float b = 1.0f;
+            float a = 1.0f;
 
             static constexpr int Size()
             {
