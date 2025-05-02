b9210e12fd1499bd4f813b93fba77e989bb4b6e6
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-05-02 09:43:45 +0300

    Update example

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,21 +1,20 @@
 #include "Application.hpp"
 #include "LinearAlgebraDataTypes.hpp"
 #include "Sphere.hpp"
-#include "Scene.hpp"
+#include "SphereObjectOpenGL.hpp"
 
 
 int main(void)
 {
-	Andromeda::Space::Scene scene;
+    Andromeda::EngineCore::Application app;
+    app.Init();
 
 	float sphereRadius = 1.0f;
 	Andromeda::Math::Vec3 spherePosition(0.0f, 0.0f, 0.0f);
-    Andromeda::Space::Sphere sphere = Andromeda::Space::Sphere(spherePosition, Andromeda::Space::Color(1.0f, 0.0f, 0.0f, 1.0f));
+    //Andromeda::Space::Sphere sphere = Andromeda::Space::Sphere(spherePosition, Andromeda::Space::Color(1.0f, 0.0f, 0.0f, 1.0f));
+	Andromeda::Rendering::SphereObjectOpenGL* sphereObjectOpenGL = new Andromeda::Rendering::SphereObjectOpenGL(sphereRadius, Andromeda::Space::Color(1.0f, 0.0f, 0.0f, 1.0f));
+	app.AddToScene(0, sphereObjectOpenGL);
 
-	scene.AddObject(1, &sphere);
-
-    Andromeda::EngineCore::Application app;
-    app.Init();
     app.RunMainLoop();
     app.DeInit();
 
