79c521b7bafd38e71b7a3dee78b5c6f73ab28a49
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-05-06 09:18:41 +0300

    Update example

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,7 +1,9 @@
 #include "Application.hpp"
 #include "LinearAlgebraDataTypes.hpp"
 #include "Sphere.hpp"
+#include "Cube.hpp"
 #include "SphereObjectOpenGL.hpp"
+#include "CubeObjectOpenGL.hpp"
 
 
 int main(void)
@@ -9,11 +11,17 @@ int main(void)
     Andromeda::EngineCore::Application app;
     app.Init();
 
-	float sphereRadius = 1.0f;
+	float sphereRadius = 0.6f;
+	float cubeHalfExtent = 0.5f;
 	Andromeda::Math::Vec3 spherePosition(0.0f, 0.0f, 0.0f);
-    //Andromeda::Space::Sphere sphere = Andromeda::Space::Sphere(spherePosition, Andromeda::Space::Color(1.0f, 0.0f, 0.0f, 1.0f));
-	Andromeda::Rendering::SphereObjectOpenGL* sphereObjectOpenGL = new Andromeda::Rendering::SphereObjectOpenGL(sphereRadius, Andromeda::Space::Color(1.0f, 0.0f, 0.0f, 1.0f));
+	Andromeda::Math::Vec3 cubePosition(0.0f, 0.0f, 0.0f);
+	Andromeda::Space::Sphere sphere = Andromeda::Space::Sphere(spherePosition, sphereRadius, Andromeda::Space::Color(0.647f, 0.647f, 1.0f, 1.0f));
+	Andromeda::Space::Cube cube = Andromeda::Space::Cube(cubePosition, cubeHalfExtent, Andromeda::Space::Color(1.0f, 0.0f, 0.0f, 1.0f));
+
+	Andromeda::Rendering::SphereObjectOpenGL* sphereObjectOpenGL = new Andromeda::Rendering::SphereObjectOpenGL(spherePosition, sphereRadius, Andromeda::Space::Color(0.647f, 0.647f, 1.0f, 1.0f));
+	Andromeda::Rendering::CubeObjectOpenGL* cubeObjectOpenGL = new Andromeda::Rendering::CubeObjectOpenGL(cubePosition, cubeHalfExtent, Andromeda::Space::Color(0.2f, 1.0f, 0.4f, 1.0f));
 	app.AddToScene(0, sphereObjectOpenGL);
+	app.AddToScene(1, cubeObjectOpenGL);
 
     app.RunMainLoop();
     app.DeInit();
