b712cbfbd34ba48ef510ce831bc429b83d66a740
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-05-24 19:30:25 +0300

    Add normal

--- Rendering/Vertices/include/Vertex.hpp
+++ Rendering/Vertices/include/Vertex.hpp
@@ -14,21 +14,28 @@ namespace Andromeda
 		{
 			Space::Point3D position;
 			Space::Color color;
+			Space::Point3D normal;
 
 			Vertex() = default;
 
 			Vertex(const Space::Point3D& pos, const Space::Color& col)
-				: position(pos), color(col)
+				: position(pos), color(col), normal(0.0f, 0.0f, 0.0f)
+			{
+			}
+
+			Vertex(const Space::Point3D& pos, const Space::Color& col, const Space::Point3D& norm)
+				: position(pos), color(col), normal(norm)
 			{
 			}
 
 			bool operator==(const Vertex& other) const
 			{
-				return position == other.position && color == other.color;
+				return position == other.position &&
+					color == other.color &&
+					normal == other.normal;
 			}
 		};
 	}
 }
-
 
 #endif // RENDERING__VERTEX__HPP
