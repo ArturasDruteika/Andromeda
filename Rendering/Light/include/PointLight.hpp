a38ac2f129d14eb1a58492a23eecfc0b4f2c0ea3
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-07-11 19:49:59 +0300

    Adds point light implementation
    Adds a point light class with attenuation and position properties.
    
    This allows for the creation of point lights in the scene.

--- Rendering/Light/include/PointLight.hpp
+++ Rendering/Light/include/PointLight.hpp
@@ -2,16 +2,34 @@
 #define RENDERING__POINT_LIGHT__HPP
 
 
+#include "Light.hpp"
 #include "glm/glm.hpp"
 
 
 namespace Andromeda::Rendering
 {
-	class PointLight
+	class PointLight : public Light
 	{
 	public:
-		PointLight();
+		PointLight(const glm::vec3& color = glm::vec3{ 1.0f }, float intensity = 1.0f);
 		~PointLight();
+
+		// Getters
+		float GetAttenuationConstant() const;
+		float GetAttenuationLinear() const;
+		float GetAttenuationQuadratic() const;
+		glm::vec3 GetPosition() const;
+		// Setters
+		void SetAttenuationConstant(float attenuationConstant);
+		void SetAttenuationLinear(float attenuationLinear);
+		void SetAttenuationQuadratic(float attenuationQuadratic);
+		void SetPosition(const glm::vec3& position);
+
+	private:
+		float m_attenuationConstant;
+		float m_attenuationLinear;
+		float m_attenuationQuadratic;
+		glm::vec3 m_position;
 	};
 }
 
--- Rendering/Light/src/PointLight.cpp
+++ Rendering/Light/src/PointLight.cpp
@@ -3,5 +3,65 @@
 
 namespace Andromeda::Rendering
 {
-        
+	PointLight::PointLight(const glm::vec3& color, float intensity)
+		: Light(color, intensity, LightType::Point)
+	{
+	}
+
+	PointLight::~PointLight() = default;
+
+    float PointLight::GetAttenuationConstant() const
+    {
+        return m_attenuationConstant;
+    }
+
+    float PointLight::GetAttenuationLinear() const
+    {
+        return m_attenuationLinear;
+    }
+
+    float PointLight::GetAttenuationQuadratic() const
+    {
+        return m_attenuationQuadratic;
+    }
+
+    glm::vec3 PointLight::GetPosition() const
+    {
+        return m_position;
+    }
+
+    void PointLight::SetAttenuationConstant(float constant)
+    {
+        if (constant < 0.0f)
+        {
+            spdlog::error("Attenuation constant must be non-negative");
+            return;
+        }
+        m_attenuationConstant = constant;
+    }
+
+    void PointLight::SetAttenuationLinear(float linear)
+    {
+        if (linear < 0 || linear > 1)
+        {
+            spdlog::error("Attenuation linear components must be between 0.0 and 1.0");
+            return;
+        }
+        m_attenuationLinear = linear;
+    }
+
+    void PointLight::SetAttenuationQuadratic(float quadratic)
+    {
+        if (quadratic < 0 || quadratic > 1)
+        {
+            spdlog::error("Attenuation quadratic components must be between 0.0 and 1.0");
+            return;
+        }
+        m_attenuationQuadratic = quadratic;
+    }
+
+    void PointLight::SetPosition(const glm::vec3& position)
+    {
+        m_position = position;
+    }
 }
