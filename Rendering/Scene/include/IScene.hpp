c047308a5e30bfc41cd7022c427872d8584cd030
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-07-27 19:10:49 +0300

    Adds proto IScene interface for scene management
    Introduces an IScene interface and its implementation
    to enable better scene management and abstraction within
    the rendering system. This allows for more flexible and
    testable scene implementations.

---
+++ Rendering/Scene/include/IScene.hpp
@@ -0,0 +1,34 @@
+#ifndef ENVIRONMENT__SCENE__HPP
+#define ENVIRONMENT__SCENE__HPP
+
+
+#if defined(_WIN32)
+	#if defined(RENDERING_EXPORT)
+		#define RENDERING_API __declspec(dllexport)
+	#else
+		#define RENDERING_API __declspec(dllimport)
+	#endif /* RENDERING_API */
+	#define _sprintf sprintf_s
+#endif
+
+#if defined(__GNUC__)
+	// GCC
+	#define RENDERING_API __attribute__((visibility("default")))
+#endif
+
+
+#include "pch.hpp"
+
+
+namespace Andromeda::Rendering
+{
+	class RENDERING_API IScene
+	{
+	public:
+		virtual ~IScene();
+		
+	};
+}
+
+
+#endif // ENVIRONMENT__ENVIRONMENT__HPP
