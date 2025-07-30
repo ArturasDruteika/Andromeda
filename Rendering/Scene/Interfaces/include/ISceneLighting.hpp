e10cc3982832fa2a07a0fc36870f4950d56f97ba
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-07-30 08:38:42 +0300

    Moves Scene interfaces to new directory
    Moves the scene interface headers and source files to a dedicated "Interfaces" subdirectory to improve project organization and clarity.
    
    Updates include paths to reflect the new directory structure, ensuring continued functionality.

--- Rendering/Scene/Interfaces/include/ISceneLighting.hpp
+++ Rendering/Scene/Interfaces/include/ISceneLighting.hpp
@@ -18,7 +18,7 @@
 
 
 #include "pch.hpp"
-#include "../../RenderableObjects/Interfaces/include/IRenderableObject.hpp"
+#include "../../../RenderableObjects/Interfaces/include/IRenderableObject.hpp"
 #include "LinearAlgebraDataTypes.hpp"
 
 
