6f7dac90083a18ca29416f66b018d43773b6467c
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-08-03 18:43:43 +0300

    Adds shader type enumeration
    Introduces an enum class for defining shader types.
    
    This will be used for shader management and compilation.

---
+++ Rendering/Shaders/include/ShaderTypes.hpp
@@ -0,0 +1,18 @@
+#ifndef RENDERER__SHADER_TYPES__HPP
+#define RENDERER__SHADER_TYPES__HPP
+
+
+#include "pch.hpp"
+
+
+namespace Andromeda::Rendering
+{
+	enum class ShaderType
+	{
+		Vertex,
+		Fragment
+	};
+}
+
+
+#endif // RENDERER__SHADER_TYPES__HPP
