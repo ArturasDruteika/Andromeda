67621590fdc67cd8f20974a8ab567e3b3cc9408c
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-08-11 20:20:02 +0300

    Centralizes macro exports for rendering API
    Refactors the rendering API macro definitions into a dedicated header file for centralized management.
    
    This change streamlines the codebase by removing redundant macro definitions from multiple header files and consolidating them into a single, reusable "MacroExports.hpp" file. This promotes consistency and simplifies future maintenance of the rendering API.

--- Rendering/RenderableObjects/Interfaces/include/ITransformable.hpp
+++ Rendering/RenderableObjects/Interfaces/include/ITransformable.hpp
@@ -2,21 +2,7 @@
 #define RENDERING__INTERFACES__I_TRANSFORMABLE__HPP
 
 
-#if defined(_WIN32)
-    #if defined(RENDERING_EXPORT)
-        #define RENDERING_API __declspec(dllexport)
-    #else
-        #define RENDERING_API __declspec(dllimport)
-    #endif /* RENDERING_API */
-    #define _sprintf sprintf_s
-#endif
-
-#if defined(__GNUC__)
-    // GCC
-    #define RENDERING_API __attribute__((visibility("default")))
-#endif
-
-
+#include "../../../MacroExports/include/MacroExports.hpp"
 #include "LinearAlgebraDataTypes.hpp"
 
 
--- Rendering/Renderers/Interfaces/include/ISizeControl.hpp
+++ Rendering/Renderers/Interfaces/include/ISizeControl.hpp
@@ -2,19 +2,7 @@
 #define RENDERER__I_SIZE__HPP
 
 
-#if defined(_WIN32)
-	#if defined(RENDERING_EXPORT)
-		#define RENDERING_API __declspec(dllexport)
-	#else
-		#define RENDERING_API __declspec(dllimport)
-	#endif /* RENDERING_API */
-	#define _sprintf sprintf_s
-#endif
-
-#if defined(__GNUC__)
-	// GCC
-	#define RENDERING_API __attribute__((visibility("default")))
-#endif
+#include "../../../MacroExports/include/MacroExports.hpp"
 
 
 namespace Andromeda::Rendering
