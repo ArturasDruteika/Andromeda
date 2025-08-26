fc98f7be76b7a28544898450b43e87893878079a
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-08-26 20:17:48 +0300

    Moves precompiled header support to a separate project

---
+++ PrecompiledHeaders/include/pch.hpp
@@ -0,0 +1,26 @@
+#ifndef PRECOMPILED_HEADERS__PCH__HPP
+#define PRECOMPILED_HEADERS__PCH__HPP
+
+
+// Standard library headers
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <functional>
+#include <fstream>
+#include <sstream>
+#include <cstdint>
+#include <stdexcept>
+#include <filesystem>
+#include <initializer_list>
+#include <array>
+#include <limits>
+
+#include "spdlog/spdlog.h"
+
+
+#endif // PRECOMPILED_HEADERS__PCH__HPP
