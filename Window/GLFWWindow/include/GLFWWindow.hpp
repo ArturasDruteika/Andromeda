ca51f2c95ce177a62fb91d2a1d0d6d8b866f8d4c
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-08-26 20:41:35 +0300

    Refactors namespaces

--- Window/GLFWWindow/include/GLFWWindow.hpp
+++ Window/GLFWWindow/include/GLFWWindow.hpp
@@ -1,5 +1,5 @@
-#ifndef GLFW_WINDOW_HPP
-#define GLFW_WINDOW_HPP
+#ifndef WINDOW__GLFW_WINDOW__HPP_HPP
+#define WINDOW__GLFW_WINDOW__HPP_HPP
 
 #include <GLFW/glfw3.h>
 #include <functional>
@@ -14,52 +14,49 @@ constexpr int DEFAULT_WINDOW_WIDTH = 640;
 constexpr int DEFAULT_WINDOW_HEIGHT = 640;
 
 
-namespace Andromeda
+namespace Andromeda::Window
 {
-	namespace Window
+	class GLFWWindow
 	{
-		class GLFWWindow
-		{
-		public:
-			using EventCallbackFn = std::function<void(Window::Event&)>;
+	public:
+		using EventCallbackFn = std::function<void(Window::Event&)>;
 
-			GLFWWindow(
-				int width = DEFAULT_WINDOW_WIDTH,
-				int height = DEFAULT_WINDOW_HEIGHT,
-				const std::string& windowName = "Andromeda Window",
-				bool initWindow = true
-			);
-			~GLFWWindow();
+		GLFWWindow(
+			int width = DEFAULT_WINDOW_WIDTH,
+			int height = DEFAULT_WINDOW_HEIGHT,
+			const std::string& windowName = "Andromeda Window",
+			bool initWindow = true
+		);
+		~GLFWWindow();
 
-			void Init();
-			void DeInit();
-			void CreateWindow();
-			void SetCallbackFunctions();
+		void Init();
+		void DeInit();
+		void CreateNewWindow();
+		void SetCallbackFunctions();
 
-			unsigned int GetWidth() const;
-			unsigned int GetHeight() const;
-			std::string GetWindowName() const;
-			bool IsInitialized() const;
-			GLFWwindow* GetWindow() const;
+		unsigned int GetWidth() const;
+		unsigned int GetHeight() const;
+		std::string GetWindowName() const;
+		bool IsInitialized() const;
+		GLFWwindow* GetWindow() const;
 
-			void SetEventCallback(const EventCallbackFn& callback);
+		void SetEventCallback(const EventCallbackFn& callback);
 
-		private:
-			// GLFW Callbacks
-			static void ResizeWindow(GLFWwindow* window, int width, int height);
-			static void WindowClose(GLFWwindow* window);
-			static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-			static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-			static void MouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
-			static void MouseMoveCallback(GLFWwindow* window, double xPos, double yPos);
+	private:
+		// GLFW Callbacks
+		static void ResizeWindow(GLFWwindow* window, int width, int height);
+		static void WindowClose(GLFWwindow* window);
+		static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+		static void MouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+		static void MouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset);
+		static void MouseMoveCallback(GLFWwindow* window, double xPos, double yPos);
 
-			int m_width, m_height;
-			std::string m_windowName;
-			GLFWwindow* m_window;
-			bool m_isInitialized;
-			EventCallbackFn m_EventCallback;
-		};
-	}
+		int m_width, m_height;
+		std::string m_windowName;
+		GLFWwindow* m_window;
+		bool m_isInitialized;
+		EventCallbackFn m_EventCallback;
+	};
 }
 
-#endif // GLFW_WINDOW_HPP
+#endif // WINDOW__GLFW_WINDOW__HPP_HPP
--- Window/GLFWWindow/src/GLFWWindow.cpp
+++ Window/GLFWWindow/src/GLFWWindow.cpp
@@ -1,213 +1,209 @@
 #include "../include/GLFWWindow.hpp"
 
 
-namespace Andromeda
+namespace Andromeda::Window
 {
-	namespace Window
+	GLFWWindow::GLFWWindow(int width, int height, const std::string& windowName, bool initWindow)
+		: m_width{ width }
+		, m_height{ height }
+		, m_windowName{ windowName }
+		, m_window{ nullptr }
+		, m_isInitialized{ false }
 	{
-		GLFWWindow::GLFWWindow(int width, int height, const std::string& windowName, bool initWindow)
-			: m_width{ width }
-			, m_height{ height }
-			, m_windowName{ windowName }
-			, m_window{ nullptr }
-			, m_isInitialized{ false }
+		if (initWindow)
 		{
-			if (initWindow)
+			Init();
+		}
+	}
+
+	GLFWWindow::~GLFWWindow()
+	{
+		DeInit();
+	}
+
+	void GLFWWindow::Init()
+	{
+		if (!m_isInitialized)
+		{
+			try
 			{
-				Init();
+				CreateNewWindow();
+				SetCallbackFunctions();
+				m_isInitialized = true;
+			}
+			catch (const std::exception& e)
+			{
+				spdlog::error("Initialization failed: {}", e.what());
+				DeInit();
 			}
 		}
+	}
 
-		GLFWWindow::~GLFWWindow()
+	void GLFWWindow::DeInit()
+	{
+		if (m_isInitialized)
 		{
-			DeInit();
+			glfwDestroyWindow(m_window);
+			m_isInitialized = false;
+		}
+	}
+
+	void GLFWWindow::CreateNewWindow()
+	{
+		m_window = glfwCreateWindow(m_width, m_height, m_windowName.c_str(), nullptr, nullptr);
+
+		if (!m_window)
+		{
+			spdlog::error("Failed to create GLFW window.");
+			return;
 		}
 
-		void GLFWWindow::Init()
+		// Associate this instance with the GLFW window
+		glfwSetWindowUserPointer(m_window, this);
+	}
+
+	void GLFWWindow::SetCallbackFunctions()
+	{
+		glfwSetFramebufferSizeCallback(m_window, ResizeWindow);
+		glfwSetWindowCloseCallback(m_window, WindowClose);
+		glfwSetKeyCallback(m_window, KeyCallback);
+		glfwSetMouseButtonCallback(m_window, MouseButtonCallback);
+		glfwSetScrollCallback(m_window, MouseScrollCallback);
+		glfwSetCursorPosCallback(m_window, MouseMoveCallback);
+	}
+
+	unsigned int GLFWWindow::GetWidth() const
+	{
+		return m_width;
+	}
+
+	unsigned int GLFWWindow::GetHeight() const
+	{
+		return m_height;
+	}
+
+	std::string GLFWWindow::GetWindowName() const
+	{
+		return m_windowName;
+	}
+
+	bool GLFWWindow::IsInitialized() const
+	{
+		return m_isInitialized;
+	}
+
+	GLFWwindow* GLFWWindow::GetWindow() const
+	{
+		return m_window;
+	}
+
+	void GLFWWindow::SetEventCallback(const EventCallbackFn& callback)
+	{
+		m_EventCallback = callback;
+	}
+
+	void GLFWWindow::ResizeWindow(GLFWwindow* window, int width, int height)
+	{
+		GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+		if (instance)
 		{
-			if (!m_isInitialized)
+			instance->m_width = width;
+			instance->m_height = height;
+			// spdlog::debug("Window resized to {}x{}", width, height);
+
+			if (instance->m_EventCallback)
 			{
-				try
+				WindowResizeEvent event(width, height);
+				instance->m_EventCallback(event);
+			}
+		}
+	}
+
+	void GLFWWindow::WindowClose(GLFWwindow* window)
+	{
+		GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+		if (instance)
+		{
+			// spdlog::debug("Window close event triggered.");
+			if (instance->m_EventCallback)
+			{
+				WindowCloseEvent event;
+				instance->m_EventCallback(event);
+			}
+		}
+	}
+
+	void GLFWWindow::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	{
+		GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+		if (instance)
+		{
+			if (instance->m_EventCallback)
+			{
+				if (action == GLFW_PRESS || action == GLFW_REPEAT)
 				{
-					CreateWindow();
-					SetCallbackFunctions();
-					m_isInitialized = true;
+					KeyPressedEvent event(static_cast<KeyCode>(key), action == GLFW_REPEAT);
+					// spdlog::debug("Key Pressed: {} (Repeat: {})", key, action == GLFW_REPEAT);
+					instance->m_EventCallback(event);
 				}
-				catch (const std::exception& e)
+				else if (action == GLFW_RELEASE)
 				{
-					spdlog::error("Initialization failed: {}", e.what());
-					DeInit();
-				}
-			}
-		}
-
-		void GLFWWindow::DeInit()
-		{
-			if (m_isInitialized)
-			{
-				glfwDestroyWindow(m_window);
-				m_isInitialized = false;
-			}
-		}
-
-		void GLFWWindow::CreateWindow()
-		{
-			m_window = glfwCreateWindow(m_width, m_height, m_windowName.c_str(), nullptr, nullptr);
-
-			if (!m_window)
-			{
-				spdlog::error("Failed to create GLFW window.");
-				return;
-			}
-
-			// Associate this instance with the GLFW window
-			glfwSetWindowUserPointer(m_window, this);
-		}
-
-		void GLFWWindow::SetCallbackFunctions()
-		{
-			glfwSetFramebufferSizeCallback(m_window, ResizeWindow);
-			glfwSetWindowCloseCallback(m_window, WindowClose);
-			glfwSetKeyCallback(m_window, KeyCallback);
-			glfwSetMouseButtonCallback(m_window, MouseButtonCallback);
-			glfwSetScrollCallback(m_window, MouseScrollCallback);
-			glfwSetCursorPosCallback(m_window, MouseMoveCallback);
-		}
-
-		unsigned int GLFWWindow::GetWidth() const
-		{
-			return m_width;
-		}
-
-		unsigned int GLFWWindow::GetHeight() const
-		{
-			return m_height;
-		}
-
-		std::string GLFWWindow::GetWindowName() const
-		{
-			return m_windowName;
-		}
-
-		bool GLFWWindow::IsInitialized() const
-		{
-			return m_isInitialized;
-		}
-
-		GLFWwindow* GLFWWindow::GetWindow() const
-		{
-			return m_window;
-		}
-
-		void GLFWWindow::SetEventCallback(const EventCallbackFn& callback)
-		{
-			m_EventCallback = callback;
-		}
-
-		void GLFWWindow::ResizeWindow(GLFWwindow* window, int width, int height)
-		{
-			GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
-			if (instance)
-			{
-				instance->m_width = width;
-				instance->m_height = height;
-				// spdlog::debug("Window resized to {}x{}", width, height);
-
-				if (instance->m_EventCallback)
-				{
-					WindowResizeEvent event(width, height);
+					KeyReleasedEvent event(static_cast<KeyCode>(key));
+					// spdlog::debug("Key Released: {}", key);
 					instance->m_EventCallback(event);
 				}
 			}
 		}
+	}
 
-		void GLFWWindow::WindowClose(GLFWwindow* window)
+	void GLFWWindow::MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+	{
+		GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+		if (instance)
 		{
-			GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
-			if (instance)
+			if (instance->m_EventCallback)
 			{
-				// spdlog::debug("Window close event triggered.");
-				if (instance->m_EventCallback)
+				if (action == GLFW_PRESS)
 				{
-					WindowCloseEvent event;
+					MouseButtonPressedEvent event(static_cast<MouseCode>(button));
+					// spdlog::debug("Mouse Button Pressed: {}", button);
+					instance->m_EventCallback(event);
+				}
+				else if (action == GLFW_RELEASE)
+				{
+					MouseButtonReleasedEvent event(static_cast<MouseCode>(button));
+					// spdlog::debug("Mouse Button Released: {}", button);
 					instance->m_EventCallback(event);
 				}
 			}
 		}
+	}
 
-		void GLFWWindow::KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+	void GLFWWindow::MouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset)
+	{
+		GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+		if (instance)
 		{
-			GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
-			if (instance)
+			if (instance->m_EventCallback)
 			{
-				if (instance->m_EventCallback)
-				{
-					if (action == GLFW_PRESS || action == GLFW_REPEAT)
-					{
-						KeyPressedEvent event(static_cast<KeyCode>(key), action == GLFW_REPEAT);
-						// spdlog::debug("Key Pressed: {} (Repeat: {})", key, action == GLFW_REPEAT);
-						instance->m_EventCallback(event);
-					}
-					else if (action == GLFW_RELEASE)
-					{
-						KeyReleasedEvent event(static_cast<KeyCode>(key));
-						// spdlog::debug("Key Released: {}", key);
-						instance->m_EventCallback(event);
-					}
-				}
+				MouseScrolledEvent event(static_cast<float>(xOffset), static_cast<float>(yOffset));
+				// spdlog::debug("Mouse Scrolled: X Offset = {}, Y Offset = {}", xOffset, yOffset);
+				instance->m_EventCallback(event);
 			}
 		}
+	}
 
-		void GLFWWindow::MouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+	void GLFWWindow::MouseMoveCallback(GLFWwindow* window, double xPos, double yPos)
+	{
+		GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
+		if (instance)
 		{
-			GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
-			if (instance)
+			if (instance->m_EventCallback)
 			{
-				if (instance->m_EventCallback)
-				{
-					if (action == GLFW_PRESS)
-					{
-						MouseButtonPressedEvent event(static_cast<MouseCode>(button));
-						// spdlog::debug("Mouse Button Pressed: {}", button);
-						instance->m_EventCallback(event);
-					}
-					else if (action == GLFW_RELEASE)
-					{
-						MouseButtonReleasedEvent event(static_cast<MouseCode>(button));
-						// spdlog::debug("Mouse Button Released: {}", button);
-						instance->m_EventCallback(event);
-					}
-				}
+				MouseMovedEvent event(static_cast<float>(xPos), static_cast<float>(yPos));
+				// spdlog::debug("Mouse Moved: X = {}, Y = {}", xPos, yPos);
+				instance->m_EventCallback(event);
 			}
 		}
-
-		void GLFWWindow::MouseScrollCallback(GLFWwindow* window, double xOffset, double yOffset)
-		{
-			GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
-			if (instance)
-			{
-				if (instance->m_EventCallback)
-				{
-					MouseScrolledEvent event(static_cast<float>(xOffset), static_cast<float>(yOffset));
-					// spdlog::debug("Mouse Scrolled: X Offset = {}, Y Offset = {}", xOffset, yOffset);
-					instance->m_EventCallback(event);
-				}
-			}
-		}
-
-		void GLFWWindow::MouseMoveCallback(GLFWwindow* window, double xPos, double yPos)
-		{
-			GLFWWindow* instance = static_cast<GLFWWindow*>(glfwGetWindowUserPointer(window));
-			if (instance)
-			{
-				if (instance->m_EventCallback)
-				{
-					MouseMovedEvent event(static_cast<float>(xPos), static_cast<float>(yPos));
-					// spdlog::debug("Mouse Moved: X = {}, Y = {}", xPos, yPos);
-					instance->m_EventCallback(event);
-				}
-			}
-		}
-
 	}
 }
