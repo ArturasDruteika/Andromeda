18d160740c598fe01ab05110fdfe550daa263af3
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-09-01 22:50:22 +0300

    Adds perspective control
    Adds a perspective control class to manage camera perspective properties such as field of view, near and far planes, and aspect ratio.
    
    This class provides functionalities to set and retrieve perspective properties, and automatically updates the projection matrix when these properties are modified.

--- Rendering/Camera/Abstracts/include/PerspectiveControl.hpp
+++ Rendering/Camera/Abstracts/include/PerspectiveControl.hpp
@@ -1,8 +1,40 @@
-#ifndef RENDERING__CAMERA__HPP
-#define RENDERING__CAMERA__HPP
+#ifndef RENDERING__PERSPECTIVE_CONTROL__HPP
+#define RENDERING__PERSPECTIVE_CONTROL__HPP
+
+
+#include "glm/glm.hpp"
+
+
+class PerspectiveControl
+{
+public:
+	PerspectiveControl();
+	PerspectiveControl(float fovDeg, float nearPlane, float farPlane, float aspect);
+	~PerspectiveControl();
+
+	// Getters
+	float GetFieldOfViewDegrees() const;
+	float GetNearPlane() const;
+	float GetFarPlane() const;
+	float GetAspect() const;
+	const glm::mat4& GetProjection() const;
+	// Setters
+	void SetFieldOfViewDegrees(float fovDeg);
+	void SetNearPlane(float nearPlane);
+	void SetFarPlane(float farPlane);
+	void SetAspect(float aspect);
+
+private:
+	void UpdateProjection();
+
+private:
+	float m_fovDeg;
+	float m_nearPlane;
+	float m_farPlane;
+	float m_aspect;
+	glm::mat4 m_projection;
+};
 
 
 
-
-
-#endif // RENDERING__CAMERA__HPP
+#endif // RENDERING__PERSPECTIVE_CONTROL__HPP
---
+++ Rendering/Camera/Abstracts/src/PerspectiveControl.cpp
@@ -0,0 +1,75 @@
+#include "../include/PerspectiveControl.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+
+PerspectiveControl::PerspectiveControl()
+	: m_fovDeg{ 45.0f }
+	, m_nearPlane{ 0.1f }
+	, m_farPlane{ 1000.0f }
+	, m_aspect{ 1.0f }
+{
+	UpdateProjection();
+}
+
+PerspectiveControl::PerspectiveControl(float fovDeg, float nearPlane, float farPlane, float aspect)
+	: m_fovDeg{ fovDeg }
+	, m_nearPlane{ nearPlane }
+	, m_farPlane{ farPlane }
+	, m_aspect{ aspect }
+{
+	UpdateProjection();
+}
+
+PerspectiveControl::~PerspectiveControl()
+{
+}
+
+float PerspectiveControl::GetFieldOfViewDegrees() const
+{
+	return m_fovDeg;
+}
+
+float PerspectiveControl::GetNearPlane() const
+{
+	return m_nearPlane;
+}
+
+float PerspectiveControl::GetFarPlane() const
+{
+	return m_farPlane;
+}
+
+float PerspectiveControl::GetAspect() const
+{
+	return m_aspect;
+}
+
+const glm::mat4& PerspectiveControl::GetProjection() const
+{
+	return m_projection;
+}
+
+void PerspectiveControl::SetFieldOfViewDegrees(float fovDeg)
+{
+	m_fovDeg = fovDeg;
+}
+
+void PerspectiveControl::SetNearPlane(float nearPlane)
+{
+	m_nearPlane = nearPlane;
+}
+
+void PerspectiveControl::SetFarPlane(float farPlane)
+{
+	m_farPlane = farPlane;
+}
+
+void PerspectiveControl::SetAspect(float aspect)
+{
+	m_aspect = aspect;
+}
+
+void PerspectiveControl::UpdateProjection()
+{
+	m_projection = glm::infinitePerspective(glm::radians(m_fovDeg), m_aspect, m_nearPlane);
+}
