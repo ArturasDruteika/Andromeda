e09ab09bd753c436c4b3c7bdb13c339e816dafd6
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-09-02 08:22:28 +0300

    Namespaces PerspectiveControl class
    Encapsulates the PerspectiveControl class within the
    Andromeda::Rendering namespace.
    
    This improves code organization and avoids potential naming
    conflicts by grouping related rendering components.

--- Rendering/Camera/Abstracts/include/PerspectiveControl.hpp
+++ Rendering/Camera/Abstracts/include/PerspectiveControl.hpp
@@ -5,36 +5,38 @@
 #include "glm/glm.hpp"
 
 
-class PerspectiveControl
+namespace Andromeda::Rendering
 {
-public:
-	PerspectiveControl();
-	PerspectiveControl(float fovDeg, float nearPlane, float farPlane, float aspect);
-	~PerspectiveControl();
+	class PerspectiveControl
+	{
+	public:
+		PerspectiveControl();
+		PerspectiveControl(float fovDeg, float nearPlane, float farPlane, float aspect);
+		~PerspectiveControl();
 
-	// Getters
-	float GetFieldOfViewDegrees() const;
-	float GetNearPlane() const;
-	float GetFarPlane() const;
-	float GetAspect() const;
-	const glm::mat4& GetProjection() const;
-	// Setters
-	void SetFieldOfViewDegrees(float fovDeg);
-	void SetNearPlane(float nearPlane);
-	void SetFarPlane(float farPlane);
-	void SetAspect(float aspect);
+		// Getters
+		float GetFieldOfViewDegrees() const;
+		float GetNearPlane() const;
+		float GetFarPlane() const;
+		float GetAspect() const;
+		const glm::mat4& GetProjection() const;
+		// Setters
+		void SetFieldOfViewDegrees(float fovDeg);
+		void SetNearPlane(float nearPlane);
+		void SetFarPlane(float farPlane);
+		void SetAspect(float aspect);
 
-private:
-	void UpdateProjection();
+	private:
+		void UpdateProjection();
 
-private:
-	float m_fovDeg;
-	float m_nearPlane;
-	float m_farPlane;
-	float m_aspect;
-	glm::mat4 m_projection;
-};
-
+	private:
+		float m_fovDeg;
+		float m_nearPlane;
+		float m_farPlane;
+		float m_aspect;
+		glm::mat4 m_projection;
+	};
+}
 
 
 #endif // RENDERING__PERSPECTIVE_CONTROL__HPP
