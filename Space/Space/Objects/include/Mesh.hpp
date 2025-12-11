8f6469b3db649b7bd78fe939bd903b661acdac6f
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-12-11 19:45:03 +0200

    Implements Mesh class
    Implements the IMesh interface in the Mesh class.
    
    This change ensures that the Mesh class adheres to the
    IMesh interface, promoting consistency and enabling polymorphism
    across mesh implementations.

--- Space/Space/Objects/include/Mesh.hpp
+++ Space/Space/Objects/include/Mesh.hpp
@@ -1,24 +1,26 @@
-#ifndef RENDERING__ABSTRACTS__MESH__HPP
-#define RENDERING__ABSTRACTS__MESH__HPP
+#ifndef ANDROMEDA__SPACE__MESH__HPP
+#define ANDROMEDA__SPACE__MESH__HPP
 
 
 #include "pch.hpp"
 #include "Space/Vertices/include/Vertex.hpp"
+#include "Andromeda/Space/Objects/IMesh.hpp"
 
 
 namespace Andromeda::Space
 {
 	class Mesh
+		: public virtual IMesh
 	{
 	public:
 		Mesh();
 		~Mesh();
 
 		// Getters
-		unsigned int GetIndexCount() const;
-		unsigned int GetVertexCount() const;
-		const std::vector<unsigned int>& GetIndices() const;
-		const std::vector<Space::Vertex>& GetVertices() const;
+		unsigned int GetIndexCount() const override;
+		unsigned int GetVertexCount() const override;
+		const std::vector<unsigned int>& GetIndices() const override;
+		const std::vector<Space::Vertex>& GetVertices() const override;
 
 	protected:
 		std::vector<unsigned int> m_indices;
@@ -27,4 +29,4 @@ namespace Andromeda::Space
 }
 
 
-#endif // RENDERING__ABSTRACTS__MESH__HPP
+#endif // ANDROMEDA__SPACE__MESH__HPP
