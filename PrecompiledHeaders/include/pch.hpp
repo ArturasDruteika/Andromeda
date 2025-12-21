4e33dc543dcd5ca6c89cee30943b3dd141b58630
ArtūrasDrūteika <arturas.druteika@gmail.com>
2025-12-22 00:48:41 +0200

    Reorders alphabetically

--- PrecompiledHeaders/include/pch.hpp
+++ PrecompiledHeaders/include/pch.hpp
@@ -3,22 +3,23 @@
 
 
 // Standard library headers
-#include <iostream>
-#include <vector>
-#include <string>
-#include <memory>
-#include <map>
-#include <unordered_map>
 #include <algorithm>
-#include <functional>
-#include <fstream>
-#include <sstream>
-#include <cstdint>
-#include <stdexcept>
-#include <filesystem>
-#include <initializer_list>
 #include <array>
+#include <cstddef>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
 #include <limits>
+#include <map>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 
 #endif // PRECOMPILED_HEADERS__PCH__HPP
